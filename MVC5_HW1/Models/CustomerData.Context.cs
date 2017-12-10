800bc550b375c8965dbcaf6d9e33d892f212363b
iris <iris@jcs.com.tw>
2017-12-10 22:19:21 +0800

    客戶聯絡資訊：新增NewCustomerController，並加入ViewModel和Index

--- MVC5_HW1/Models/CustomerData.Context.cs
+++ MVC5_HW1/Models/CustomerData.Context.cs
@@ -29,5 +29,7 @@ namespace MVC5_HW1.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+
+        public System.Data.Entity.DbSet<MVC5_HW1.Models.ViewModel.NewCustomerData> NewCustomerDatas { get; set; }
     }
 }
