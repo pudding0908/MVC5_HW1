98c9811edf0b24b875dce79e038b34076862018b
iris <iris@jcs.com.tw>
2017-12-13 11:52:41 +0800

    -修改頁籤顯示，主選單增加客戶管理 -客戶管理內容頁中，增加連結前往三個主要功能畫面 -客戶資料、客戶聯絡人、客戶銀行資訊皆加入關鍵字搜尋條件

---
+++ MVC5_HW1/Controllers/MainCustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5_HW1.Models;
+
+namespace MVC5_HW1.Controllers
+{
+    public class MainCustomerController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: MainCustomer
+        public ActionResult Index()
+        {
+            return View(db.客戶聯絡人與銀行帳戶數量.ToList());
+        }
+    }
+}
